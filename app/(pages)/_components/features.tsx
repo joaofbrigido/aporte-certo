d625eae1dfc0cb1ee2ff4342d6378f2288509642
João Fernando do Amaral Brigido <70164946+joaofbrigido@users.noreply.github.com>
2025-08-20 11:16:44 -0300

    add imagens na LP

---
+++ app/(pages)/_components/features.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { cn } from "@/app/lib/utils";
+import { ChartCandlestick, HandCoins, Percent } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+export const Features = () => {
+  const features = [
+    {
+      title: "Preço de Aportes",
+      icon: <HandCoins />,
+      description:
+        "Otimize suas compras e maximize os lucros com base em suas estratégias de investimento",
+      active: false,
+      image: "/aporte.jpg",
+    },
+    {
+      title: "Preço Teto",
+      icon: <ChartCandlestick />,
+      description:
+        "Determine o preço máximo que você deve pagar por uma ação para manter a rentabilidade desejada",
+      active: false,
+      image: "/preco-teto.jpg",
+    },
+    {
+      title: "Juros Compostos",
+      icon: <Percent />,
+      description:
+        "Simule o crescimento do seu patrimônio ao longo do tempo com base nos aportes regulares e na taxa de rendimento",
+      active: false,
+      image: "/juros-compostos.jpg",
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div className="flex items-center justify-between gap-5 mt-16 max-lg:flex-col">
+      <ul className="space-y-4">
+        {features.map((feature, index) => (
+          <li
+            key={feature.title + index}
+            onClick={() => setActiveIndex(index)}
+            className={cn(
+              "p-5 rounded-lg hover:bg-accent transition cursor-pointer",
+              activeIndex === index && "bg-accent border"
+            )}
+          >
+            <h4 className="font-semibold text-lg flex gap-3 items-center">
+              {feature.icon}
+              {feature.title}
+            </h4>
+            <p className="mt-4 text-muted-foreground">{feature.description}</p>
+          </li>
+        ))}
+      </ul>
+
+      <div className="w-2/3 max-lg:w-full">
+        <Image
+          src={features[activeIndex].image}
+          alt={features[activeIndex].title}
+          width={1000}
+          height={900}
+          className="object-contain border-8 rounded-2xl h-[450px] max-md:h-[300px]"
+        />
+      </div>
+    </div>
+  );
+};
--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,14 +1,7 @@
 import Link from "next/link";
 import { Button } from "../components/ui/button";
-import {
-  ArrowUpRight,
-  ChartCandlestick,
-  HandCoins,
-  Percent,
-} from "lucide-react";
-import { Card, CardContent } from "../components/ui/card";
+import { ArrowUpRight } from "lucide-react";
 import SectionTitle from "./_components/section-title";
-import { cn } from "../lib/utils";
 import { PlansCard } from "./_components/plans-card";
 import { PlansCardItem } from "./_components/plans-card-item";
 import {
@@ -18,6 +11,8 @@ import {
   AccordionTrigger,
 } from "../components/ui/accordion";
 import { ComparisonCard } from "./_components/comparison-card";
+import Image from "next/image";
+import { Features } from "./_components/features";
 
 export default function Home() {
   const faqs = [
@@ -58,8 +53,8 @@ export default function Home() {
   ];
 
   return (
-    <div className="mainContainer m-5 relative">
-      <main className="pt-11">
+    <>
+      <main className="pt-12 mainContainer relative">
         <h1 className="text-5xl font-bold text-center max-w-3xl m-auto max-md:text-3xl leading-16 max-md:leading-10 ">
           Ferramentas Essenciais para{" "}
           <span className="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-primary before:rounded">
@@ -86,145 +81,105 @@ export default function Home() {
 
         <div className="absolute left-1/2 -translate-x-1/2 -z-100 w-[90%] h-60 bg-primary blur-[85px] opacity-50 dark:opacity-40 rounded-full" />
 
-        <Card className="h-[450px] mt-16">
-          <CardContent></CardContent>
-        </Card>
+        <Image
+          src="/hero-ap.png"
+          alt="Hero"
+          width={1400}
+          height={950}
+          className="mt-16 rounded-3xl w-full object-cover border-8 mx-auto shadow-2xl"
+          priority
+        />
       </main>
 
-      <section className="mt-36 bg-stone-800 p-16 rounded-4xl max-sm:px-5">
-        <div className="text-center">
-          <h2 className="text-md text-primary mb-1 uppercase font-medium">
-            É MUUUUITA VANTAGEM
-          </h2>
-          <h3 className="text-3xl font-bold text-white">
-            Aporte tradicional vs usando a{" "}
-            <span className="text-primary">Aporte Certo</span>
-          </h3>
-        </div>
+      <section className="bg-stone-800 pb-28 pt-44 px-5 -mt-16">
+        <div className="mainContainer">
+          <div className="text-center">
+            <h2 className="text-md text-primary mb-1 uppercase font-medium">
+              É MUUUUITA VANTAGEM
+            </h2>
+            <h3 className="text-3xl font-bold text-white">
+              Aporte tradicional vs usando a{" "}
+              <span className="text-primary">Aporte Certo</span>
+            </h3>
+          </div>
 
-        <div className="mt-16 grid grid-cols-2 gap-5 max-[940px]:grid-cols-1">
-          <ComparisonCard
-            title="Tradicional"
-            list={[
-              "+2h escolhendo ativos para aportar",
-              "+4h Cálculando preço teto",
-              "+1h Fazendo simulações",
-              "Sem organização de preços = menor rentabilidade",
-              "Sem planejamento dos ativos = menor rentabilidade",
-              "Sem controle de sua carteira = menor rentabilidade",
-            ]}
+          <div className="mt-16 grid grid-cols-2 gap-5 max-[940px]:grid-cols-1">
+            <ComparisonCard
+              title="Tradicional"
+              list={[
+                "+2h escolhendo ativos para aportar",
+                "+4h Cálculando preço teto",
+                "+1h Fazendo simulações",
+                "Sem organização de preços = menor rentabilidade",
+                "Sem planejamento dos ativos = menor rentabilidade",
+                "Sem controle de sua carteira = menor rentabilidade",
+              ]}
+            />
+            <ComparisonCard
+              highline
+              title="Com Aporte Certo"
+              list={[
+                "Organização minimalista de seus investimentos",
+                "Planejamento otimizado na escolha de ativos",
+                "Aumento de rentabilidade e economia de tempo com organização de aportes",
+              ]}
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="mainContainer">
+        <section>
+          <SectionTitle
+            title="Funcionalidades"
+            subtitle="Descubra as Nossas Calculadoras Poderosas"
           />
-          <ComparisonCard
-            highline
-            title="Com Aporte Certo"
-            list={[
-              "Organização otimizada de seus investimentos",
-              "Planejamento minimalista na escolha de ativos",
-              "Aumento de rentabilidade e economia de tempo com organização de aportes",
-            ]}
-          />
-        </div>
-      </section>
 
-      <section>
-        <SectionTitle
-          title="Funcionalidades"
-          subtitle="Descubra as Nossas Calculadoras Poderosas"
-        />
+          <Features />
+        </section>
 
-        <div className="flex items-center justify-between gap-5 mt-16 max-lg:flex-col">
-          <ul className="space-y-4">
-            <li
-              className={cn(
-                "p-5 rounded-lg hover:bg-accent transition cursor-pointer",
-                true && "bg-accent border"
-              )}
-            >
-              <h4 className="font-semibold text-lg flex gap-3 items-center">
-                <ChartCandlestick />
-                Preço Teto
-              </h4>
-              <p className="mt-4 text-muted-foreground">
-                Determine o preço máximo que você deve pagar por uma ação para
-                manter a rentabilidade desejada
-              </p>
-            </li>
-            <li
-              className={cn(
-                "p-5 rounded-lg hover:bg-accent transition cursor-pointer",
-                false && "bg-accent border"
-              )}
-            >
-              <h4 className="font-semibold text-lg flex gap-3 items-center">
-                <Percent />
-                Juros Compostos
-              </h4>
-              <p className="mt-4 text-muted-foreground">
-                Simule o crescimento do seu patrimônio ao longo do tempo com
-                base nos aportes regulares e na taxa de rendimento
-              </p>
-            </li>
-            <li
-              className={cn(
-                "p-5 rounded-lg hover:bg-accent transition cursor-pointer",
-                false && "bg-accent border"
-              )}
-            >
-              <h4 className="font-semibold text-lg flex gap-3 items-center">
-                <HandCoins />
-                Preço de Aportes
-              </h4>
-              <p className="mt-4 text-muted-foreground">
-                Otimize suas compras e maximize os lucros com base em suas
-                estratégias de investimento
-              </p>
-            </li>
-          </ul>
+        <section>
+          <SectionTitle title="Planos" subtitle="Invista em Suas Decisões" />
 
-          <Card className="w-2/3 h-[500px] border-8 min-w-[500px] max-lg:min-w-full">
-            <CardContent></CardContent>
-          </Card>
-        </div>
-      </section>
+          <div className="grid grid-cols-2 gap-5 mt-16 max-md:grid-cols-1">
+            <PlansCard name="Free" price="R$0,00" plan="free">
+              <PlansCardItem title="Acesso a todas as novas calculadoras" />
+              <PlansCardItem title="Sem anúncios" notSupport />
+              <PlansCardItem title="Mantem os dados salvos" notSupport />
+              <PlansCardItem title="Suporte via email" notSupport />
+              <PlansCardItem
+                title="Exportação de tabelas em excel"
+                notSupport
+              />
+            </PlansCard>
 
-      <section>
-        <SectionTitle title="Planos" subtitle="Invista em Suas Decisões" />
+            <PlansCard name="Pro" price="R$69,00" plan="pro" popular>
+              <PlansCardItem title="Acesso a todas as novas calculadoras" />
+              <PlansCardItem title="Sem anúncios" />
+              <PlansCardItem title="Mantem os dados salvos" />
+              <PlansCardItem title="Suporte via email" />
+              <PlansCardItem title="Exportação de tabelas em excel" />
+            </PlansCard>
+          </div>
+        </section>
 
-        <div className="grid grid-cols-2 gap-5 mt-16 max-md:grid-cols-1">
-          <PlansCard name="Free" price="R$0,00" plan="free">
-            <PlansCardItem title="Acesso a todas as novas calculadoras" />
-            <PlansCardItem title="Sem anúncios" notSupport />
-            <PlansCardItem title="Mantem os dados salvos" notSupport />
-            <PlansCardItem title="Suporte via email" notSupport />
-            <PlansCardItem title="Exportação de tabelas em excel" notSupport />
-          </PlansCard>
+        <section>
+          <SectionTitle title="Faq" subtitle="Dúvidas Frequentes" />
 
-          <PlansCard name="Pro" price="R$69,00" plan="pro" popular>
-            <PlansCardItem title="Acesso a todas as novas calculadoras" />
-            <PlansCardItem title="Sem anúncios" />
-            <PlansCardItem title="Mantem os dados salvos" />
-            <PlansCardItem title="Suporte via email" />
-            <PlansCardItem title="Exportação de tabelas em excel" />
-          </PlansCard>
-        </div>
-      </section>
-
-      <section>
-        <SectionTitle title="Faq" subtitle="Dúvidas Frequentes" />
-
-        <Accordion type="single" collapsible className="mt-16">
-          {faqs.map((faq) => (
-            <AccordionItem value={`faq-${faq.id}`} key={`FAQ-${faq.id}`}>
-              <AccordionTrigger className="cursor-pointer text-lg">
-                {faq.question}
-              </AccordionTrigger>
-              <AccordionContent className="text-base">
-                {faq.answer}
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </section>
-    </div>
+          <Accordion type="single" collapsible className="mt-16">
+            {faqs.map((faq) => (
+              <AccordionItem value={`faq-${faq.id}`} key={`FAQ-${faq.id}`}>
+                <AccordionTrigger className="cursor-pointer text-lg">
+                  {faq.question}
+                </AccordionTrigger>
+                <AccordionContent className="text-base">
+                  {faq.answer}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </section>
+      </div>
+    </>
   );
 }
